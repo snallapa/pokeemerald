8c15d2acc5bc5e5eb1aeb5ae74c80f71a81e7fa5
Sahith Nallapareddy <sahith.reddy@gmail.com>
2019-12-27 17:04:22 -0500

    new starters

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -126,9 +126,9 @@ static const u8 sStarterLabelCoords[][2] =
 
 static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
-    SPECIES_TREECKO,
-    SPECIES_TORCHIC,
     SPECIES_MUDKIP,
+    SPECIES_TOTODILE,
+    SPECIES_SQUIRTLE,
 };
 
 static const struct BgTemplate gUnknown_085B1E00[3] =
