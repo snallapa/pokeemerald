a969fbbab9152a303c3b629cd3555382b2142f2a
Sahith Nallapareddy <sahith.reddy@gmail.com>
2019-12-28 12:32:46 -0500

    no replacing

--- songs.mk
+++ songs.mk
@@ -255,9 +255,6 @@ $(MID_SUBDIR)/bop.s: %.s: %.mid
 $(MID_SUBDIR)/falls_down.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G019 -V080
 
-$(MID_SUBDIR)/carti.s: %.s: %.mid
-	$(MID) $< $@ -E -R$(STD_REVERB) -G019 -V080
-
 $(MID_SUBDIR)/mus_machupi.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G080 -V080
 
