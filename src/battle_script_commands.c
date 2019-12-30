006988330699097e26b555f3dc462a08b42d3dd3
Sahith Nallapareddy <sahith.reddy@gmail.com>
2019-12-30 17:31:01 -0500

    do these show

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -10203,6 +10203,7 @@ static void Cmd_handleballthrow(void)
 
 static void Cmd_givecaughtmon(void)
 {
+    printf("HERE");
     if (GiveMonToPlayer(&gEnemyParty[0]) != MON_GIVEN_TO_PARTY)
     {
         if (!ShouldShowBoxWasFullMessage())
