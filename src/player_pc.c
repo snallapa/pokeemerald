434ba4fe73eab25584bfd3aa08dac8307cfb10f4
Sahith Nallapareddy <sahith.reddy@gmail.com>
2020-01-01 20:32:56 -0500

    test

--- src/player_pc.c
+++ src/player_pc.c
@@ -170,7 +170,7 @@ static const struct MenuAction gPCText_ItemPCOptionsText[] =
 static const struct ItemSlot gNewGamePCItems[] =
 {
     { ITEM_POTION, 1 },
-    { ITEM_TRADE_STONE, 1 },
+    { ITEM_KINGS_ROCK, 1},
     { ITEM_NONE, 0 }
 };
 
--- src/starter_choose.c
+++ src/starter_choose.c
@@ -127,7 +127,7 @@ static const u8 sStarterLabelCoords[][2] =
 static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
     SPECIES_MUDKIP,
-    SPECIES_TOTODILE,
+    SPECIES_POLIWHIRL,
     SPECIES_SQUIRTLE,
 };
 
