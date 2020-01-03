ffebaf03a9394334ccfcc827a77510b97281ce5b
Sahith Nallapareddy <sahith.reddy@gmail.com>
2020-01-03 13:39:35 -0500

    finished trade stone

--- src/data/item_icon_table.h
+++ src/data/item_icon_table.h
@@ -110,8 +110,8 @@ const u32 *const gItemIconTable[][2] =
     [ITEM_THUNDER_STONE] = {gItemIcon_ThunderStone, gItemIconPalette_ThunderStone},
     [ITEM_WATER_STONE] = {gItemIcon_WaterStone, gItemIconPalette_WaterStone},
     [ITEM_LEAF_STONE] = {gItemIcon_LeafStone, gItemIconPalette_LeafStone},
+    [ITEM_TRADE_STONE] = {gItemIcon_TradeStone, gItemIconPalette_TradeStone},
     // ????????
-    [ITEM_063] = {gItemIcon_QuestionMark, gItemIconPalette_QuestionMark},
     [ITEM_064] = {gItemIcon_QuestionMark, gItemIconPalette_QuestionMark},
     [ITEM_065] = {gItemIcon_QuestionMark, gItemIconPalette_QuestionMark},
     [ITEM_066] = {gItemIcon_QuestionMark, gItemIconPalette_QuestionMark},
