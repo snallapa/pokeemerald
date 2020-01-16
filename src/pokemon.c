5de1a8f0d216a0b2f0860f38eb23af683e6f07f1
Sahith Nallapareddy <sahith.reddy@gmail.com>
2020-01-15 22:07:29 -0500

    omg

--- src/pokemon.c
+++ src/pokemon.c
@@ -4325,7 +4325,7 @@ u8 GiveMonToPlayer(struct Pokemon *mon)
 
     CopyMon(&gPlayerParty[i], mon, sizeof(*mon));
     ball = ITEM_SHADOW_BALL;
-    SetMonData(&gPlayerParty[i], MON_DATA_POKEBALL, &ball)
+    SetMonData(&gPlayerParty[i], MON_DATA_POKEBALL, &ball);
     gPlayerPartyCount = i + 1;
     return MON_GIVEN_TO_PARTY;
 }
