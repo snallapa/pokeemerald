4bfaf2c1004b18f80310722948ab7a67623ec409
Sahith Nallapareddy <sahith.reddy@gmail.com>
2020-02-23 00:05:05 -0500

    fix #11 added TRAINER HEX who gives mystic ticket to ho oh and lugia

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -874,11 +874,12 @@
 #define TRAINER_FRANK                       870
 #define TRAINER_KENNY                       871
 #define TRAINER_CHANCE                      872
+#define TRAINER_HEX                         873
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 9 additional trainers before trainer flag space overflows
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
 //       MAX_TRAINERS_COUNT can be increased but will take up additional saveblock space
 
-#define TRAINERS_COUNT                      873
+#define TRAINERS_COUNT                      874
 #define MAX_TRAINERS_COUNT                  864
 #define TRAINERS_FLAG_NO                    0x356 //original ones
 
