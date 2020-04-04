55d7f4d08dc18cb002f78710f48d1b8d902cd386
Sahith Nallapareddy <sahith.reddy@gmail.com>
2020-04-04 15:35:02 -0400

    updated to upstream

--- include/constants/event_objects.h
+++ include/constants/event_objects.h
@@ -247,6 +247,7 @@
 #define NUM_OBJ_EVENT_GFX                        242
 
 
+
 // These are dynamic object gfx ids.
 // They correspond with the values of the VAR_OBJ_GFX_ID_X vars.
 // More info about them in include/constants/vars.h
